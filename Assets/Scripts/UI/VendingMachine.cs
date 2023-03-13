c19b10b24eccb2bbfdb80694b37f4ea5a8e534a0
Jiang Zhimeng <anyaasai@gmail.com>
2023-03-13 17:06:50 +0800

    Finish VendingMachine

--- Assets/Scripts/UI/VendingMachine.cs
+++ Assets/Scripts/UI/VendingMachine.cs
@@ -8,6 +8,11 @@ public class VendingMachine : MonoBehaviour
     [SerializeField]
     GameObject[] items;
     GameObject currentItem;
+    [SerializeField]
+    GameObject flicker;
+    [SerializeField]
+    GameObject text;
+    bool hasSelected;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,13 +22,23 @@ public class VendingMachine : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!Input.GetKeyDown("left") && !Input.GetKeyDown("right")) {
+        if (hasSelected) {
             return;
         }
-        
-        if (Input.GetKeyDown("left")) 
+
+        if (Input.GetButtonDown("Submit")) {
+            hasSelected = true;
+            StartCoroutine(Selected());
+        }
+
+        if (!(Input.GetKeyDown("left") || Input.GetKeyDown(KeyCode.A))
+            && !(Input.GetKeyDown("right") || Input.GetKeyDown(KeyCode.D))) {
+            return;
+        }
+
+        if (Input.GetKeyDown("left") || Input.GetKeyDown(KeyCode.A)) 
             index--;
-        if (Input.GetKeyDown("right"))
+        if (Input.GetKeyDown("right") || Input.GetKeyDown(KeyCode.D))
             index++;
         
         if (index >= items.Length) {
@@ -36,4 +51,17 @@ public class VendingMachine : MonoBehaviour
         currentItem = items[index];
         currentItem.SetActive(true);
     }
+
+    IEnumerator Selected() {
+        text.SetActive(true);
+        for (int i = 0; i <= 4; i++) {
+            flicker.SetActive(true);
+            yield return new WaitForSeconds(0.05f);
+            flicker.SetActive(false);
+            yield return new WaitForSeconds(0.02f);
+        }
+        text.SetActive(false);
+        yield return null;
+        hasSelected = false;
+    } 
 }
